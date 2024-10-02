fde9d826a31e155cd3a4dd8f906f8f10af9d0a05
codemeleon <anmol.kiran@gmail.com>
2024-10-02 16:04:53 +0100

    change color of selected

--- riboseqorg/main/static/js/RiboSeqOrg.js
+++ riboseqorg/main/static/js/RiboSeqOrg.js
@@ -1,6 +1,23 @@
-function buildURL(item)
-{
-  item.href=baseurl+window.location.href;
-  return true;
+function buildURL(item) {
+        item.href = baseurl + window.location.href;
+        return true;
 };
 
+
+
+$(function() {
+        const x = new URLSearchParams(window.location.search);
+        const vals = [];
+        for (const [key, value] of x) {
+                console.log(`${key}: ${value}`);
+                vals.push(value);
+        }
+        $("a.sidenav-link").each(function() {
+                const a_val = $(this).html().trim();
+                if (vals.indexOf(a_val) > -1) {
+
+                        $(this).css("background-color", "red");
+                }
+        });
+
+});
