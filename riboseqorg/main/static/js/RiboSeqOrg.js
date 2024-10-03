c2d521e8a485ac4814b4ff72ac8ddf357cc90881
codemeleon <anmol.kiran@gmail.com>
2024-10-03 11:18:48 +0100

    show Filters

--- riboseqorg/main/static/js/RiboSeqOrg.js
+++ riboseqorg/main/static/js/RiboSeqOrg.js
@@ -6,14 +6,22 @@ function buildURL(item) {
 $(function () {
   const x = new URLSearchParams(window.location.search);
   const vals = [];
+  const filters = [];
   for (const [key, value] of x) {
     console.log(`${key}: ${value}`);
     vals.push(value);
+    const filter = key + "=" + value;
+    if (filters.indexOf(filter) == -1) {
+      filters.push(filter);
+    }
   }
+  const filters_str = "Filters: " + filters.join("&");
   $("a.sidenav-link").each(function () {
     const a_val = $(this).html().trim();
     if (vals.indexOf(a_val) > -1) {
       $(this).css("color", "red");
     }
   });
+
+  $("#filters").html(filters_str);
 });
