69dc3e853cafd9f611fa1e2f0352d7f28b855d79
codemeleon <anmol.kiran@gmail.com>
2024-10-03 08:10:34 +0100

    replaced background to text color

--- riboseqorg/main/static/js/RiboSeqOrg.js
+++ riboseqorg/main/static/js/RiboSeqOrg.js
@@ -1,23 +1,19 @@
 function buildURL(item) {
-        item.href = baseurl + window.location.href;
-        return true;
-};
+  item.href = baseurl + window.location.href;
+  return true;
+}
 
-
-
-$(function() {
-        const x = new URLSearchParams(window.location.search);
-        const vals = [];
-        for (const [key, value] of x) {
-                console.log(`${key}: ${value}`);
-                vals.push(value);
-        }
-        $("a.sidenav-link").each(function() {
-                const a_val = $(this).html().trim();
-                if (vals.indexOf(a_val) > -1) {
-
-                        $(this).css("background-color", "red");
-                }
-        });
-
+$(function () {
+  const x = new URLSearchParams(window.location.search);
+  const vals = [];
+  for (const [key, value] of x) {
+    console.log(`${key}: ${value}`);
+    vals.push(value);
+  }
+  $("a.sidenav-link").each(function () {
+    const a_val = $(this).html().trim();
+    if (vals.indexOf(a_val) > -1) {
+      $(this).css("color", "red");
+    }
+  });
 });
