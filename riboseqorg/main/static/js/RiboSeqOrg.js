4fba44a905485c82d0d1c90c7c4baf0f5cf84b57
codemeleon <anmol.kiran@gmail.com>
2024-10-10 21:22:37 +0100

    filter removal refinements

--- riboseqorg/main/static/js/RiboSeqOrg.js
+++ riboseqorg/main/static/js/RiboSeqOrg.js
@@ -9,13 +9,21 @@ $(function () {
   const filters = [];
   for (const [key, value] of x) {
     vals.push(value);
-    const filter = "<a class='key'>" + key + "=" + value + "</a>";
+    const filter =
+      "<li>" +
+      value +
+      " <bold style='color:red'><a class='key' data-value='" +
+      key +
+      "=" +
+      value +
+      "'>×</a></bold></li>";
     if (filters.indexOf(filter) == -1) {
       filters.push(filter);
     }
   }
   if (vals.length != 0) {
-    const filters_str = "Filters:" + filters.join(" ");
+    const filters_str =
+      "<ul class='key2'><li>Filters:</li>" + filters.join(" ") + "</ul>";
     $("a.sidenav-link").each(function () {
       const a_val = $(this).html().trim();
       if (vals.indexOf(a_val) > -1) {
@@ -29,15 +37,17 @@ $(function () {
 
 $(document).ready(function () {
   $(".key").click(function () {
-    const to_remove = $(this).html();
+    // const to_remove = $(this).html();
+    const to_remove = $(this).data("value");
     const vals = [];
     $(".key").each(function () {
-      const a_val = $(this).html().trim();
+      const a_val = $(this).data("value");
       if (a_val != to_remove) {
         vals.push(a_val);
       }
     });
     const new_url = window.location.pathname + "?" + vals.join("&");
-    window.open(new_url);
+    window.location.href = new_url;
+    // window.open(new_url);
   });
 });
